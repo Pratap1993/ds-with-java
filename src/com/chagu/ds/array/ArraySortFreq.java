188fa9f864bf4d85229f50419d35589a23dd574a
Pratap1993 <dhal.pratapbhanu@gmail.com>
2022-06-16 10:47:59 +0530

    Updated Array Examples

--- src/com/chagu/ds/array/ArraySortFreq.java
+++ src/com/chagu/ds/array/ArraySortFreq.java
@@ -10,16 +10,24 @@ public class ArraySortFreq {
 
     public static void main(String[] args) {
         int[] input = {4, 3, 1, 6, 4, 3, 6, 4};
+        int[] output = sortArrayInTermsOfRepeatFrequency(input);
+        String inputStr = Arrays.toString(input);
+        System.out.println(inputStr);
+        String returnType = Arrays.toString(output);
+        System.out.println(returnType);
+    }
+
+    //A method to sort an array based on repetition frequency descending-ly
+    private static int[] sortArrayInTermsOfRepeatFrequency(int[] input) {
         int[] output = new int[input.length];
-        Map<Integer, Integer> mymap = new HashMap<>();
-        for (int i = 0; i < input.length; i++) {
-            if (mymap.containsKey(input[i])) {
-                mymap.replace(input[i], mymap.get(input[i]) + 1);
-            } else {
-                mymap.put(input[i], 1);
-            }
+        Map<Integer, Integer> elementCountMap = new HashMap<>();
+        for (int i : input) {
+            if (elementCountMap.containsKey(i))
+                elementCountMap.replace(i, elementCountMap.get(i) + 1);
+            else
+                elementCountMap.put(i, 1);
         }
-        Map<Integer, Integer> sortedMap = mymap.entrySet().stream()
+        Map<Integer, Integer> sortedMap = elementCountMap.entrySet().stream()
                 .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
         int position = 0;
@@ -29,9 +37,6 @@ public class ArraySortFreq {
                 position++;
             }
         }
-        String inputStr = Arrays.toString(input);
-        System.out.println(inputStr);
-        String returnType = Arrays.toString(output);
-        System.out.println(returnType);
+        return output;
     }
 }
--- src/com/chagu/ds/array/OneDimensionArray.java
+++ src/com/chagu/ds/array/OneDimensionArray.java
@@ -2,71 +2,72 @@ package com.chagu.ds.array;
 
 public class OneDimensionArray {
 
-	int[] arr = null;
+    private final int[] arr;
 
-	public OneDimensionArray(int arraySize) {
-		arr = new int[arraySize];
-		for (int i = 0; i < arraySize; i++)
-			arr[i] = Integer.MIN_VALUE;
-	}
+    public OneDimensionArray(int arraySize) {
+        arr = new int[arraySize];
+        for (int i = 0; i < arraySize; i++)
+            arr[i] = Integer.MIN_VALUE;
+    }
 
-	public void insert(int location, int value) {
-		if (arr == null)
-			throw new RuntimeException("Array is no longer exist !!!");
-		if (arr.length - 1 < location || location < 0)
-			throw new ArrayIndexOutOfBoundsException("Invalid index to insert data !!!");
-		arr[location] = value;
-	}
+    public void insert(int location, int value) {
+        if (arr.length - 1 < location || location < 0)
+            throw new ArrayIndexOutOfBoundsException("Invalid index to insert data !!!");
+        arr[location] = value;
+    }
 
-	public void safeInsert(int location, int value) {
-		if (arr == null)
-			throw new RuntimeException("Array is no longer exist !!!");
-		if (arr.length - 1 < location || location < 0)
-			throw new ArrayIndexOutOfBoundsException("Invalid index to insert data !!!");
-		if (arr[location] > Integer.MIN_VALUE)
-			throw new RuntimeException("Accessed index is already occupied !!!");
-		arr[location] = value;
-	}
+    public void safeInsert(int location, int value) {
+        if (arr.length - 1 < location || location < 0)
+            throw new ArrayIndexOutOfBoundsException("Invalid index to insert data !!!");
+        if (arr[location] > Integer.MIN_VALUE)
+            throw new RuntimeException("Accessed index is already occupied !!!");
+        arr[location] = value;
+    }
 
-	public int accessIndex(int location) {
-		if (arr == null)
-			throw new RuntimeException("Array is no longer exist !!!");
-		if (arr.length - 1 < location || location < 0)
-			throw new ArrayIndexOutOfBoundsException("Invalid index to access data !!!");
-		return arr[location];
-	}
+    public int accessIndex(int location) {
+        if (arr.length - 1 < location || location < 0)
+            throw new ArrayIndexOutOfBoundsException("Invalid index to access data !!!");
+        return arr[location];
+    }
 
-	public int linearSearchValue(int value) {
-		if (arr == null)
-			throw new RuntimeException("Array is no longer exist !!!");
-		int indexToReturn = -1;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == value) {
-				indexToReturn = i;
-				break;
-			}
-		}
-		if (indexToReturn == -1)
-			throw new RuntimeException("Data not found !!!");
-		return indexToReturn;
-	}
+    public int linearSearchValue(int value) {
+        int indexToReturn = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                indexToReturn = i;
+                break;
+            }
+        }
+        if (indexToReturn == -1)
+            throw new RuntimeException("Data not found !!!");
+        return indexToReturn;
+    }
 
-	@Override
-	public String toString() {
-		if (arr == null)
-			throw new RuntimeException("Array is no longer exist !!!");
-		if (arr.length == 0)
-			return "[ ]";
-		StringBuffer sb = new StringBuffer();
-		sb.append("[");
-		for (int i = 0; i < arr.length; i++) {
-			if (i == arr.length - 1)
-				sb.append(arr[i]);
-			else
-				sb.append(arr[i] + ", ");
-		}
-		sb.append("]");
-		return sb.toString();
-	}
+    @Override
+    public String toString() {
+        if (arr.length == 0)
+            return "[ ]";
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1)
+                sb.append(arr[i]);
+            else
+                sb.append(arr[i]).append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        OneDimensionArray oneDArray = new OneDimensionArray(5);
+        for (int i = 0; i < oneDArray.arr.length; i++)
+            oneDArray.arr[i] = (i + 1) * 10;
+        System.out.println(oneDArray);
+        System.out.println(oneDArray.accessIndex(4));
+        System.out.println(oneDArray.linearSearchValue(50));
+        oneDArray.insert(2, 25);
+        System.out.println(oneDArray);
+    }
 
 }
