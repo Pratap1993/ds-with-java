f4b449ba88f7257035b3a148e94586c7126a752b
Pratap1993 <dhal.pratapbhanu@gmail.com>
2022-12-02 23:59:21 +0530

    Search and access methods added in 2Darray

--- src/com/chagu/ds/array/TwoDimensionArray.java
+++ src/com/chagu/ds/array/TwoDimensionArray.java
@@ -39,6 +39,25 @@ public class TwoDimensionArray {
         System.out.println("Value overridden");
     }
 
+    public int accessCell(int row, int column) {
+        validateRowColumn(row, column);
+        if (arr[row][column] == Integer.MIN_VALUE)
+            throw new RuntimeException("Cell is not assigned.");
+        return arr[row][column];
+    }
+
+    public void searchValue(int value) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                if (arr[row][column] == value) {
+                    System.out.println("Value " + value + " found at [" + row + "][" + column + "]");
+                    return;
+                }
+            }
+        }
+        System.out.println("Value " + value + " is not present in the array.");
+    }
+
     private void validateRowColumnSize(int rowSize, int columnSize) {
         if (rowSize < 1 && columnSize < 1)
             throw new RuntimeException("Row size and column size must be grater than zero.");
@@ -51,15 +70,18 @@ public class TwoDimensionArray {
 
     public static void main(String[] args) {
         TwoDimensionArray twoDimensionArray = new TwoDimensionArray(3, 3);
-        twoDimensionArray.insertValue(0,0, 10);
-        twoDimensionArray.insertValue(0,1, 10);
-        twoDimensionArray.insertValue(0,2, 10);
-        twoDimensionArray.insertValue(1,0, 10);
-        twoDimensionArray.insertValue(1,1, 10);
-        twoDimensionArray.insertValue(1,2, 10);
-        twoDimensionArray.insertValue(2,0, 10);
-        twoDimensionArray.insertValue(2,1, 10);
-        twoDimensionArray.insertValue(2,2, 10);
+        twoDimensionArray.insertValue(0, 0, 10);
+        twoDimensionArray.insertValue(0, 1, 10);
+        twoDimensionArray.insertValue(0, 2, 10);
+        twoDimensionArray.insertValue(1, 0, 10);
+        twoDimensionArray.insertValue(1, 1, 10);
+        twoDimensionArray.insertValue(1, 2, 10);
+        twoDimensionArray.insertValue(2, 0, 10);
+        twoDimensionArray.insertValue(2, 1, 10);
+        twoDimensionArray.insertValue(2, 2, 10);
+        twoDimensionArray.overrideValue(2, 2, 25);
         twoDimensionArray.traverseTheArray();
+        System.out.println("Value at cell number [2][2] is " + twoDimensionArray.accessCell(2, 2));
+        twoDimensionArray.searchValue(25);
     }
 }
